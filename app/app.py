c70b248c34841ebafa7cd803e5d68ffbe949dcea
Pratham3301 <s.pratham0100@gmail.com>
2025-01-26 11:21:33 +0530

    Testing CI/CD pipeline with a small change

--- app/app.py
+++ app/app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return 'Hello, this is a test for CI/CD pipeline!_________'
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
