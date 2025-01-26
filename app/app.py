4b810fa14108ac0d87ad2047e5092c76030e6f8e
Pratham3301 <s.pratham0100@gmail.com>
2025-01-26 11:25:24 +0530

    Testing CI/CD with a small change

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, this is a test for CI/CD pipeline!_________'
+    return 'Hello, this is a test for CI/CD pipeline!_____'
 
 
 if __name__ == "__main__":
